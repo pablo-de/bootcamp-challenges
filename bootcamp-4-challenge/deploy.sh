1aebffc7df4e9c3c8148c7257413fcd561854a95
Pablo <94941513+pablo-de@users.noreply.github.com>
2022-10-03 10:46:16 -0300

    Delete bootcamp-4-challenge directory

--- bootcamp-4-challenge/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker build -t ci-cd .
-
-docker run -d --name cd-cd-challenge -p 8888:8888 ci-cd
-
-docker tag ci-cd pablode24/cd-cd-challenge
-
-docker push pablode24/cd-cd-challenge
